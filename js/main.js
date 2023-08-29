c7bfffbcf967c97062d84e7d2795a11453fc1680
Erandy <vdyerand@gmail.com>
2023-08-29 06:05:18 -0600

    Se subio el proyecto

--- js/main.js
+++ js/main.js
@@ -1,52 +1,65 @@
-function validar(){
-    let nombre = document.getElementById("nombre").value;
-    let email = document.getElementById("email").value;
-    let tel = document.getElementById("tel").value;
-    let mensaje = document.getElementById("mensaje").value;
+/* Formulario */
 
-    //RegExp
+function validar() {
+    let nombre = document.getElementById("nombre");
+    let email = document.getElementById("email");
+    let mensaje = document.getElementById("mensaje");
 
-    let regexNombre =  /^([A-Za-zÑñÁáÉéÍíÓóÚú]+['-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+)(\s+([A-Za-zÑñÁáÉéÍíÓóÚú]+['-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+))*$/;
+    let regexNombre = /^([A-Za-zÑñÁáÉéÍíÓóÚú]+['-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+)(\s+([A-Za-zÑñÁáÉéÍíÓóÚú]+['-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+([A-Za-zÑñÁáÉéÍíÓóÚú]{3,40}\s*)))*$/;
     let regexCorreo = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    let regexTel =   /^[0-9]{10}$/;
-    let regexMensaje = /^([A-Za-zÑñÁáÉéÍíÓóÚú]+['-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+)(\s+([A-Za-zÑñÁáÉéÍíÓóÚú]+['-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+))*$/; 
-   
-    //Variables boolean
-    let nombreBoolean= true;
-    let emailBoolean = true;
-    let telBoolean = true;
-    let mensajeBoolean = true;
-
-    //Validar
-    if (!regexNombre.test(nombre)){
-        alert("Nombre invalido");
-        return false;
-    }
-    else if (!regexCorreo.test(email)){
-        alert("Email invalido");
-        return false;
-    }
-    else if (!regexTel.test(tel)){
-        alert("Telefono no es valido");
-        return false;
-    }
-    else if (!regexMensaje.test(mensaje)){
-        alert("Mensaje invalido");
-        return false;}
-    }
+    let regexMensaje = /^(?!(\d)\1+$)[\s\S]{10,250}$/;
 
 
-    /* ANIMACION */
-    let app = document.getElementById('typewriter');
- 
-let typewriter = new Typewriter(app, {
-  loop: true,
-  delay: 75,
-});
- 
-typewriter
-  .pauseFor(2500)
-  .typeString('Desarrollo sitios web y soy diseñadora UX UI.')
-  .pauseFor(200)
-  .deleteChars(10)
-  .start();
+
+    let nombreValid = regexNombre.test(nombre.value);
+    let emailValid = regexCorreo.test(email.value);
+    let mensajeValid = regexMensaje.test(mensaje.value);
+
+    let errorMessages = [];
+
+    if (nombre.value.trim() === "") {
+        errorMessages.push("El campo Nombre no puede estar vacío.");
+        nombre.style.borderColor = "red";
+    } else if (!nombreValid) {
+        errorMessages.push("El nombre tiene que tener de 3 a 40 caracteres y solo debe contener letras y espacios.");
+        nombre.style.borderColor = "red";
+    } else {
+        nombre.style.borderColor = "green";
+    }
+
+    if (email.value.trim() === "") {
+        errorMessages.push("El campo Correo Electrónico no puede estar vacío.");
+        email.style.borderColor = "red";
+    } else if (!emailValid) {
+        errorMessages.push("El correo electrónico solo debe contener letras, números, puntos y guiones.");
+        email.style.borderColor = "red";
+    } else {
+        email.style.borderColor = "green";
+    }
+
+
+    if (mensaje.value.trim() === "") {
+        errorMessages.push("El campo Comentarios no puede estar vacío.");
+        mensaje.style.borderColor = "red";
+    } else if (!mensajeValid) {
+        errorMessages.push("El mensaje debe ser de 10 a 250 caracteres y solo debe contener números y letras (no repetidos más de 4 veces), puede contener caracteres especiales.");
+        mensaje.style.borderColor = "red";
+    } else {
+        mensaje.style.borderColor = "green";
+    }
+
+    if (errorMessages.length > 0) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Corrige los siguientes errores:',
+            html: errorMessages.join("<br>"),
+        });
+        return false;
+    } else {
+        Swal.fire({
+            icon: 'success',
+            title: 'Tus datos serán enviados correctamente a Colibrí',
+        });
+        return true;
+    }
+}
